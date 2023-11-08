6c20bd5244735d27f226ad694492e84d02991b0a
wwwrt <tpripon99@gmail.com>
2023-11-08 18:30:08 +0200

    done

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        'google': ['', ''],
+        'facebook': [],
+        'insta': [],
+        'fundbox': [],
+      },
+    },
+  },
+  plugins: [],
+}
+
